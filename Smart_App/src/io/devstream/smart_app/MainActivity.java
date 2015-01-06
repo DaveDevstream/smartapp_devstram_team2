371c300db5cd5a36bc90d61e821441f7b3953bb9
Gary Chadwick <Gary_Chadwick@ymail.com>
2015-01-06 12:54:48 +0000

    Testing
    Just a test of Git.

--- Smart_App/src/io/devstream/smart_app/MainActivity.java
+++ Smart_App/src/io/devstream/smart_app/MainActivity.java
@@ -2,8 +2,7 @@ package io.devstream.smart_app;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+
 
 public class MainActivity extends Activity {
 
@@ -11,5 +10,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //Test Comment
     }
 }
