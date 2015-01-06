7934421453580c4f817d956e6e2de5103a8d33d3
gary1 <gary.devstream@gmail.com>
2015-01-06 14:24:11 +0000

    New Commit!!

--- Smart_App/src/io/devstream/smart_app/MainActivity.java
+++ Smart_App/src/io/devstream/smart_app/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        //Test Comment
+        // I'm working on a new push!!
     }
 }
