5dfefe2ab4346651b189e90dbf3231c9e6dadfbb
dave_costello <david.costello.devstream@gmail.com>
2015-01-06 15:14:25 +0000

    daves changes
    yo

--- Smart_App/src/io/devstream/smart_app/MainActivity.java
+++ Smart_App/src/io/devstream/smart_app/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         
         // I'm working on a new push!!
+        //this working yet.
     }
 }
